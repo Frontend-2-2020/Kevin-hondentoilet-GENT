841c89f3008d5f0189c3e6fb6f17b278a6b1ffdc
Kevin Vanantwerpen <kevin.v1989@gmail.com>
2019-10-07 21:45:14 +0200

    Final result

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,38 @@
+import axios from 'Axios';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+const map = L.map('map', {
+    //Where the map is centered (location)
+    center: [51.051123, 3.713242],
+    //Zoom ratio
+    zoom: 13
+});
+
+const pawIcon = L.icon({
+	iconUrl: './public/poo.png',
+	iconSize:     [50, 55], // size of the icon
+	iconAnchor:   [25, 55], // point of the icon which will correspond to marker's location 
+	popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+//Generates the map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//Call Gent DB hondentoiletten
+axios.get('https://datatank.stad.gent/4/infrastructuur/hondenvoorzieningen.geojson')
+  .then(response => {
+    const {coordinates} = response.data;
+
+    //Call function to set all markers
+    setMarkers(coordinates);
+  });
+
+// Function to set all 
+const setMarkers = (data) =>{
+  data.forEach((el) => {
+    L.marker([el[1],el[0]], {icon: pawIcon}).addTo(map);
+    console.log(el);
+  });
+};
