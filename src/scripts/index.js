1fd043cbe21209d4bca925e8ffbde5af80d42a33
Kevin Vanantwerpen <kevin.v1989@gmail.com>
2019-10-16 19:12:49 +0200

    new version

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,75 +1,159 @@
-import axios from 'Axios';
-import geodist from 'geodist';
+//TODO set precision of location
+//TODO hovereffect + select
+
+// import packages
+import axios from 'axios';
+import {
+  getDistance
+} from 'geolib';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+
+// import styles
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.scss';
 
-var userPos = {};
-let distances = [];
-
-const map = L.map('map', {
+let map, stdMarker, nearestMarker, userMarker;
+const generateMap = () => {
+  map = L.map('myMap', {
     //Where the map is centered (location)
     center: [51.051123, 3.713242],
     //Zoom ratio
     zoom: 13
-});
+  });
 
-//Create icon / markerImg
-const pawIcon = L.icon({
-	iconUrl: './public/poo.png',
-	iconSize:     [50, 55], // size of the icon
-	iconAnchor:   [25, 55], // point of the icon which will correspond to marker's location 
-	popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
+  //Generates the map
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Hondentoilet in Gent'
+  }).addTo(map);
+};
 
-//Generates the map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+//Create the custom markers
+const generateMarkers = () => {
+  //Create standard marker
+  stdMarker = L.icon({
+    iconUrl: './public/icons/pin-blue.png',
+    iconSize: [50, 68], // size of the icon
+    iconAnchor: [25, 68], // point of the icon which will correspond to marker's location 
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
 
-//Call Gent DB hondentoiletten
-//Set markers and next ask for position user
+  //Create nearest marker
+  nearestMarker = L.icon({
+    iconUrl: './public/icons/pin-red.png',
+    iconSize: [50, 68], // size of the icon
+    iconAnchor: [25, 68], // point of the icon which will correspond to marker's location 
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
+
+  //Create user marker
+  userMarker = L.icon({
+    iconUrl: './public/icons/pin-user.png',
+    iconSize: [50, 68], // size of the icon
+    iconAnchor: [25, 68], // point of the icon which will correspond to marker's location 
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
+};
+
+const setMarkers = (lat, long, marker) => {
+  L.marker([lat, long], {
+    icon: marker
+  }).addTo(map);
+};
+
+//Generate the map and markers
+let coordsToilets;
+const accessToiletDb = () =>{
 axios.get('https://datatank.stad.gent/4/infrastructuur/hondenvoorzieningen.geojson')
-  .then(response => {
-    const {coordinates} = response.data;
-    //Call function to set all markers
-    setMarkers(coordinates);
+  .then(function (response) {
+    // handle success
+    coordsToilets = response.data.coordinates;
+    coordsToilets.forEach((el) => {
+      setMarkers(el[1], el[0], stdMarker);
+    });
 
-    //Ask for location & set marker
-    navigator.geolocation.getCurrentPosition(success);
-    calculateDistance(coordinates);
-
-  });
-
-  // Function to set (all) markers
-const setMarkers = (data) =>{
-  data.forEach((el) => {
-    L.marker([el[1],el[0]], {icon: pawIcon}).addTo(map);
-  });
-};
-
-const success = (pos) =>{
-  const {latitude, longitude} = pos.coords;
-  L.marker([latitude,longitude], {icon: pawIcon}).addTo(map);
-  centerToLocation(latitude, longitude);
-  userPos.latitude = latitude;
-  userPos.longitude = longitude;
-};
-
-const centerToLocation = (lat, long) =>{
-  map.panTo(new L.LatLng(lat, long));
-};
-
-const calculateDistance = (dataDb) =>{
-  console.log(userPos);
-
-  dataDb.forEach((el) => {
-    // const toilet = {
-    //   'latitude': el[1],
-    //   'longitude': el[0]
-    // };
-    // const dist = geodist(userPos, toilet);
-    //const toiletData = {};
-
-    //Add to new array
-    console.log(el[0],el[1]);
+    calculateDistance();
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
   });
 };
+
+let userPosition;
+const success = (pos) => {
+  userPosition = {
+    lat: pos.coords.latitude,
+    long: pos.coords.longitude
+  };
+
+  //Set custom marker on users position & center
+  setMarkers(userPosition.lat, userPosition.long, userMarker);
+  map.panTo(new L.LatLng(userPosition.lat, userPosition.long));
+  calculateDistance();
+  console.log(userPosition);
+
+};
+
+const getUserPosition = () => {
+  navigator.geolocation.getCurrentPosition(success);
+};
+
+let distanceToilets = [];
+const calculateDistance = () => {
+  if (userPosition && coordsToilets) {
+    coordsToilets.forEach((el) => {
+      let dist = getDistance([userPosition.lat, userPosition.long], [el[1], el[0]]);
+      dist = (dist / 1000).toFixed(2);
+
+      const toilet = {
+        lat: el[1],
+        long: el[0],
+        distance: dist
+      };
+      distanceToilets.push(toilet);
+    });
+
+    sortArrayDistance(distanceToilets);
+    fillData(distanceToilets);
+    console.log(distanceToilets);
+  }
+};
+
+const sortArrayDistance = (x) => {
+  x.sort(function (a, b) {
+    if (a.distance < b.distance) {
+      return -1;
+    };
+    if (a.distance > b.distance) {
+      return 1;
+    };
+    return 0;
+  });
+};
+
+const fillData = (data) => {
+  const listGroup = document.querySelector('#data');
+  for (let i = 0; i < 5; i++) {
+    const listLink = document.createElement('a');
+    listLink.setAttribute('href', '#');
+    listLink.setAttribute('data-id', `link${i+1}`);
+    listLink.setAttribute('class', 'list-group-item list-group-item-action');
+    listLink.innerHTML =
+      `<div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Hondentoilet ${i+1}</h5>
+          <small class="text-muted">${data[i].distance} km</small>
+       </div>
+       <p class="mb-1">Position: <br>Latitude: ${data[i].lat} <br>Longitude: ${data[i].long}</p>
+       <small class="text-muted">Donec id elit non mi porta.</small>`;
+    listGroup.appendChild(listLink);
+  };
+};
+
+generateMap();
+generateMarkers();
+accessToiletDb();
+getUserPosition();
